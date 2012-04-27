47a5e89f0785e604cc37312b069862f5ed8dada0
Thomas Koch <thomas@koch.ro>
2012-04-27 16:01:14 +0200

    some customizations

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,12 +8,18 @@
  '(TeX-source-correlate-mode t)
  '(TeX-view-program-list (quote (("Okular" "okular -unique %o#src:%n`pwd`/./%b"))))
  '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Okular") (output-html "xdg-open"))))
+ '(calendar-week-start-day 1)
  '(color-theme-selection "Taming Mr Arneson" nil (color-theme_seldefcustom))
- '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-drill)))
+ '(indicate-empty-lines t)
+ '(mouse-avoidance-mode (quote banish) nil (avoid))
+ '(org-drill-optimal-factor-matrix (quote ((1 (2.1799999999999997 . 3.72) (2.6 . 4.14) (2.5 . 4.0) (1.7000000000000002 . 3.44)))))
+ '(org-modules (quote (org-bbdb org-bibtex org-crypt org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-drill)))
  '(require-final-newline nil)
  '(rfcview-index-location "/usr/share/doc/RFC/rfc-index.txt.gz")
  '(rfcview-rfc-location-pattern (quote ("/usr/share/doc/RFC/links/rfc%s.txt.gz" "/usr/share/doc/RFC/links/rfc%s.txt")))
- '(safe-local-variable-values (quote ((eval progn (flyspell-mode 1) (outline-minor-mode 1) (goto-address-mode 1) (hide-body)) (eval progn (flyspell-mode 1) (outline-minor-mode 1) (hide-body)) (eval flyspell-mode 1) (ispell-dictionary . "american") (eval hide-body)))))
+ '(safe-local-variable-values (quote ((eval progn (flyspell-mode 1) (outline-minor-mode 1) (goto-address-mode 1) (hide-body)) (eval progn (flyspell-mode 1) (outline-minor-mode 1) (hide-body)) (eval flyspell-mode 1) (ispell-dictionary . "american") (eval hide-body))))
+ '(show-trailing-whitespace t)
+ '(x-select-enable-clipboard t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
