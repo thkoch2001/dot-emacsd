d43555646fc1585c5d073a5bac5f398604ff55a4
Thomas Koch <thomas@koch.ro>
2012-08-30 17:36:57 +0200

    enable org-special-ctrl-k/a/e

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -19,7 +19,6 @@
  '(lpr-command "xpp")
  '(mouse-avoidance-mode (quote banish) nil (avoid))
  '(org-drill-optimal-factor-matrix (quote ((1 (2.1799999999999997 . 3.72) (2.6 . 4.14) (2.5 . 4.0) (1.7000000000000002 . 3.44)))))
- '(org-modules (quote (org-bbdb org-bibtex org-crypt org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-drill)))
  '(org-export-latex-classes (quote (("article" "\\documentclass[11pt]{article}
 \\usepackage[utf8]{inputenc}
 \\usepackage[T1]{fontenc}
@@ -61,6 +60,9 @@
 \\geometry{a4paper, top=25mm, left=35mm, right=25mm, bottom=30mm,
 headsep=10mm, footskip=12mm}
  " ("\\section{%s}" . "\\section*{%s}") ("\\subsection{%s}" . "\\subsection*{%s}") ("\\subsubsection{%s}" . "\\subsubsection*{%s}") ("\\paragraph{%s}" . "\\paragraph*{%s}") ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))))
+ '(org-modules (quote (org-bbdb org-bibtex org-crypt org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-drill)))
+ '(org-special-ctrl-a/e t)
+ '(org-special-ctrl-k t)
  '(require-final-newline nil)
  '(rfcview-index-location "/usr/share/doc/RFC/rfc-index.txt.gz")
  '(rfcview-rfc-location-pattern (quote ("/usr/share/doc/RFC/links/rfc%s.txt.gz" "/usr/share/doc/RFC/links/rfc%s.txt")))
