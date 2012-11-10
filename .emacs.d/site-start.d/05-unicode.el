1bd8ebc12b10d3638749729ceb6f475ebb0923c3
Thomas Koch <thomas@koch.ro>
2012-11-10 11:20:54 +0100

    add unicode settings

---
+++ .emacs.d/site-start.d/05-unicode.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;
+;; set up unicode
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; This from a japanese individual.  I hope it works.
+(setq default-buffer-file-coding-system 'utf-8)
+;; From Emacs wiki
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+(set-clipboard-coding-system 'utf-8)
