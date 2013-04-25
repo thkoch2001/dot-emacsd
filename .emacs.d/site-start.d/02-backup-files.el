5012b8d7fbe3c0355b817c406b0567228d07b972
Thomas Koch <thomas@koch.ro>
2013-04-25 19:55:21 +0200

    update backup code

--- .emacs.d/site-start.d/02-backup-files.el
+++ .emacs.d/site-start.d/02-backup-files.el
@@ -1,26 +1,26 @@
-;; always make backups by copying
-(setq backup-by-copying t)
-
 ;; backups in ~/.emacs.d/backups
-(let ((backup-dir (concat dotfiles-dir "/backups")))
+(let
+ ( (backup-dir (concat dotfiles-dir "/backups"))
+   (week (* 60 60 24 7))
+   (current (float-time (current-time)))
+   )
+ (cl-flet*
+ ((file-last-modified (file)
+    (float-time (fifth (file-attributes file))))
+
+  (should-delete (file)
+    (and (backup-file-name-p file)
+       (> (- current (file-last-modified file)) week)))
+ )
   (if (not (file-exists-p backup-dir))
         (make-directory backup-dir t))
 
   (setq backup-directory-alist `(("." . ,backup-dir)))
 
   (message "Deleting old backup files...")
-  (let ((week (* 60 60 24 7))
-       (current (float-time (current-time))))
-    (dolist (file (directory-files backup-dir t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message file)
-      (delete-file file)))
-  )
-)
 
-(setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
+  (dolist (file (directory-files backup-dir t))
+    (when (should-delete file)
+      (message file)
+      (delete-file file)
+))))
--- .emacs.d/site-start.d/03-auto-safe.el
+++ .emacs.d/site-start.d/03-auto-safe.el
@@ -1,7 +1,33 @@
-(let ((auto-save-dir (concat dotfiles-dir "auto-save/")))
-  (if (not (file-exists-p auto-save-dir))
-        (make-directory auto-save-dir t))
+;; compare 02-backup-files.el
+(let
+  ( (auto-save-dir (concat dotfiles-dir "auto-save/"))
+    (week (* 60 60 24 7))
+    (current (float-time (current-time)))
+  )
 
-  (setq auto-save-file-name-transforms
-      `((".*" ,auto-save-dir t)))
-)
+  (setq auto-save-file-name-transforms `((".*" ,auto-save-dir t)))
+
+ (cl-flet*
+ ((file-last-modified (file)
+    (float-time (fifth (file-attributes file))))
+
+  (should-delete (file)
+    (> (- current (file-last-modified file)) week))
+ )
+
+  (message "Deleting old save-list files...")
+  (dolist (file (directory-files auto-save-dir t))
+    (when (should-delete file)
+      (message file)
+      (delete-file file)
+   ))
+
+   (dolist (file (directory-files (file-name-directory auto-save-list-file-name) t))
+    (when (should-delete file)
+      (message file)
+      (delete-file file)
+   ))
+))
+
+
+
