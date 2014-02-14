ab0297664825ede6f03222a3ad6aec6764c00167
Thomas Koch <thomas@koch.ro>
2014-02-14 18:43:42 +0100

    start auto-complete mode for web-mode

--- .emacs.d/site-start.d/27-auto-complete-mode.el
+++ .emacs.d/site-start.d/27-auto-complete-mode.el
@@ -2,3 +2,6 @@
 (add-to-list 'ac-dictionary-directories "/usr/share/auto-complete/dict/")
 (require 'auto-complete-config)
 (ac-config-default)
+
+# see https://github.com/fxbois/web-mode/issues/116
+(add-to-list 'ac-modes 'web-mode)
