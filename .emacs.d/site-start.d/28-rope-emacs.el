e9d4bfa7f814f2e81ee52d3f6a15ed4885d3b9a7
Thomas Koch <thomas@koch.ro>
2016-10-05 19:56:11 +0200

    rm rope-emacs

--- .emacs.d/site-start.d/28-rope-emacs.el
+++
@@ -1,9 +0,0 @@
-(defun load-ropemacs ()
-  "Load pymacs and ropemacs"
-  (interactive)
-  (require 'pymacs)
-  (pymacs-load "ropemacs" "rope-")
-  ;; Automatically save project python buffers before refactorings
-  (setq ropemacs-confirm-saving 'nil)
-)
-(global-set-key "\C-xpl" 'load-ropemacs)
