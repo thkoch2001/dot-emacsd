7b2b00f51cd77f47e9d02863155dc146f135117a
Thomas Koch <thomas@koch.ro>
2016-10-14 13:57:33 +0200

    rm initsplit again

--- .emacs.d/custom.d/web-mode.el
+++
@@ -1,9 +0,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-enable-comment-keywords (quote ("TODO" "FIXME")))
- '(web-mode-script-padding 2)
- '(web-mode-style-padding 2)
- '(web-mode-tag-auto-close-style 2))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,13 +3,7 @@
 (debian-pkg-add-load-path-item (concat dotfiles-dir "local-lisp"))
 (debian-pkg-add-load-path-item (concat dotfiles-dir "custom-themes"))
 
-(require 'initsplit)
-
 (setq custom-file (concat dotfiles-dir "custom.el"))
-
-(let ((user-customize-d-dir (concat dotfiles-dir "/custom.d")))
-    (debian-run-directories user-customize-d-dir))
-
 (load custom-file)
 
 (let ((user-site-start-dir (concat dotfiles-dir "/site-start.d")))
