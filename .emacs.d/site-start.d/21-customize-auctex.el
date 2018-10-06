b9b53593333692fcf2fd77646691ca74c3a1c207
Thomas Koch <thomas@koch.ro>
2018-10-06 17:59:58 +0200

    .

--- .emacs.d/site-start.d/21-customize-auctex.el
+++
@@ -1,6 +0,0 @@
-;; Use pdfLaTeX. Take this out to compile to DVI, instead.
-;; already set per customize: (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
-
-;; http://www.gnu.org/s/auctex/manual/reftex/Creating-Citations.html
-;; reload the bibliography if JabRef changed it.
-(add-hook 'bibtex-mode-hook 'turn-on-auto-revert-mode)
--- .emacs.d/site-start.d/27-auto-complete-mode.el
+++
@@ -1,7 +0,0 @@
-(require 'auto-complete)
-(add-to-list 'ac-dictionary-directories "/usr/share/auto-complete/dict/")
-(require 'auto-complete-config)
-(ac-config-default)
-
-# see https://github.com/fxbois/web-mode/issues/116
-(add-to-list 'ac-modes 'web-mode)
